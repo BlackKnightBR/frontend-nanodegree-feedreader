843a49cdfb1230e0571f9ccf726920fb64ff2dd6
Rorigo Montebello Saboya Brito <blackknight@blackknightstudio.com.br>
2017-09-13 00:19:37 -0300

    Adição de comentários e de segundo suite de testes.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,71 +1,59 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+
         it('Are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-         it('Should check all the urls and validate existance of content', function() {
-           for (var i = 0; i < allFeeds.length; i++) {
-             expect(allFeeds[i].url).toBeDefined();
-             expect(allFeeds[i].url.length).not.toBe(0);
-           }
-         });
-
-         it('Should check all the names and validate existance of content', function() {
-           for (var i = 0; i < allFeeds.length; i++) {
-             expect(allFeeds[i].name).toBeDefined();
-             expect(allFeeds[i].name.length).not.toBe(0);
-           }
-         });
+        //Testes de URLs.
+        it('Should check all the urls and validate existance of content', function() {
+          //Loop que passa por toda variável allFeeds e verifica a existência de um "campo" url
+          //e garante que exista conteúdo.
+          for (var i = 0; i < allFeeds.length; i++) {
+            expect(allFeeds[i].url).toBeDefined();
+            expect(allFeeds[i].url.length).not.toBe(0);
+          }
+        });
+        //Testes de Nomes.
+        it('Should check all the names and validate existance of content', function() {
+          //Loop que passa por toda variável allFeeds e verifica a existência de um "campo" name
+          //e garante que exista conteúdo.
+          for (var i = 0; i < allFeeds.length; i++) {
+            expect(allFeeds[i].name).toBeDefined();
+            expect(allFeeds[i].name.length).not.toBe(0);
+          }
+        });
     });
 
-
+    //Suite de teste do menu e suas funcionalidades.
     describe('The Menu',function() {
-      var body = $('body'),
+      //Variáveis, 'body' é associada ao corpo do html e 'iconMenu'
+      //é associada ao ícone de menu.
+       var body = $('body'),
        iconMenu = $('.menu-icon-link');
-
-   it('Hidden by default', function() {
-     expect(body.hasClass('menu-hidden')).toBe(true);
-   });
-
-   it('Visibility changes when clicked', function() {
-     var currentClass = body.attr('class')
-         expectedNewClass = (body.hasClass('menu-hidden')) ? '' : 'menu-hidden';
-
-     iconMenu.click();
-     expect(body.attr('class')).toBe(expectedNewClass);
-
-     iconMenu.click();
-     expect(body.attr('class')).toBe(currentClass);
-   });
+    //Teste para verificar se por padrão o menu permanece escondido.
+    it('Hidden by default', function() {
+      expect(body.hasClass('menu-hidden')).toBe(true);
     });
+    //Teste para verificar se o status de visibilidade realmente muda quando o
+    //menu é clickado.
+    it('Visibility changes when clicked', function() {
+      //Variáveis, 'defaultClass' guarda atributo de classe atual de menu(definida por padrão) e
+      //'newClass' guarda teste de atributo que o menu vai assumir (oposto ao padrão sempre!).
+      var defaultClass = body.attr('class')
+          newClass = (body.hasClass('menu-hidden')) ? '' : 'menu-hidden';
+      //Determina se o estado do menu é a nova classe definida na variável
+      //'newClass' após primeiro click.
+      iconMenu.click();
+      expect(body.attr('class')).toBe(newClass);
+
+      //Determina se a o estado do menu é a classe definida por padrão (defaultClass)
+      //após segundo click.
+      iconMenu.click();
+      expect(body.attr('class')).toBe(defaultClass);
+    });
+  });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
@@ -75,6 +63,21 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+    describe('Initial Entries',function() {
+        beforeEach(function(done) {
+          loadFeed(0, function() {
+            done();
+          });
+        });
+
+        it('have at least one entry', function(done) {
+          var numEntries = $('.feed .entry').length;
+          console.log(numEntries);
+          expect(numEntries).toBeGreaterThan(0);
+          done();
+        });
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
